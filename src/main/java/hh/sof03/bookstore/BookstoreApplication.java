9ed60b1a8dfcd622bd9e260ebbb250a2b69f119d
Lauri Hynninen <laurihyn@gmail.com>
2024-09-16 19:40:14 +0300

    Chapter 3 task 1

--- src/main/java/hh/sof03/bookstore/BookstoreApplication.java
+++ src/main/java/hh/sof03/bookstore/BookstoreApplication.java
@@ -1,13 +1,26 @@
 package hh.sof03.bookstore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import hh.sof03.bookstore.domain.Book;
+import hh.sof03.bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookstoreApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreApplication.class, args);
+    }
+
+    @Bean
+CommandLineRunner runner(BookRepository bookRepository) {
+    return args -> {
+        bookRepository.save(new Book("title", "author", 0, "123456789", 0));
+        bookRepository.save(new Book("Title", "author", 0, "isbn", 0));
+    };
+}
 
 }
--- src/main/java/hh/sof03/bookstore/domain/Book.java
+++ src/main/java/hh/sof03/bookstore/domain/Book.java
@@ -1,6 +1,16 @@
 package hh.sof03.bookstore.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     private String title;
     private String author;
@@ -16,8 +26,15 @@ public class Book {
         this.publicationYear = publicationYear;
         this.isbn = isbn;
         this.price = price;
+    }
 
-}
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getTitle() {
         return title;
@@ -61,7 +78,7 @@ public class Book {
 
     @Override
     public String toString() {
-        return "Book [title=" + title + ", author=" + author + ", publicationYear=" + publicationYear + ", isbn=" + isbn
-                + ", price=" + price + "]";
-    }}
-
+        return "Book [id=" + id + ", title=" + title + ", author=" + author + ", publicationYear=" + publicationYear
+                + ", isbn=" + isbn + ", price=" + price + "]";
+    }
+}
